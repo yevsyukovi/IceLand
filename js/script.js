8d878c6fe7cb0abf9f7a0f8ee1ad3663defc27e7
yevsyukovi <87486444+yevsyukovi@users.noreply.github.com>
2021-07-16 08:15:45 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+// NAVIGATION
+
+let burgerButton = document.getElementById('menu__icon');
+let openClose = document.getElementById('openClose');
+let menu__logo = document.getElementById('menu__logo');
+let menu__body = document.getElementById('menu__body');
+
+burgerButton.onclick = function(){
+
+if(burgerButton.dataset.trigger == 'false'){
+
+        openClose.innerHTML = 'ЗАКРЫТЬ';
+        menu__body.style.left = '0%';
+        openClose.style.color = 'black';
+        menu__logo.style.border = '1px solid black';
+        burgerButton.style.color = 'black';
+        burgerButton.dataset.trigger = true;
+
+    }else{
+
+        openClose.innerText = openClose.dataset.text;
+        menu__body.style.left = '100%';
+        openClose.style.color = 'white';
+        menu__logo.style.border = '1px solid white';
+        burgerButton.style.color = 'white';
+        burgerButton.dataset.trigger = false;
+
+        }
+
+};
+
+
+// ITEM LIST 1
 
 let item__title__btn_1 = document.getElementById('item__title__btn_1');
 let tour__list_1 = document.getElementById('tour__list_1');
